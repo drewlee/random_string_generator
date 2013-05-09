5b25dc1284fd7ed5dcf619d790f8146aa88ccc54
Andrew Lee <starsthatfell@gmail.com>
2013-05-09 01:49:11 -0700

    getting rid of the stupid

--- random_string_generator.js
+++ random_string_generator.js
@@ -1,25 +1,29 @@
-(function(w){
+(function(global){
+'use strict';
+
 var chars = {};
 
-chars.num = '0 1 2 3 4 5 6 7 8 9'.split(' ');
-chars.alphalc = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(' ');
-chars.alphauc = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(' ');
-chars.alpha = chars.alphalc.concat(chars.alphauc);
-chars.alphanum = chars.alpha.concat(chars.num);
-chars.special = '! @ # $ % ^ & * ( ) - _ = + ` ~ [ ] { } \\ | ; : \' " < > . , / ?'.split(' ');
-chars.all = chars.alphanum.concat(chars.special);
+chars.num      = '0123456789';
+chars.alphalc  = 'abcdefghijklmnopqrstuvwxyz';
+chars.alphauc  = chars.alphalc.toUpperCase();
+chars.alpha    = chars.alphalc + chars.alphauc;
+chars.alphanum = chars.alpha + chars.num;
+chars.special  = '!@#$%^&*()-_=+`~[]{}\\|;:\'"<>.,/?';
+chars.all      = chars.alphanum + chars.special;
 
-w.generateRandomString = function(type, max, fixed){
-	type = type in chars ? type : 'all';
-	
-	var str = '',
-		len = fixed ? max : Math.ceil(Math.random() * (max || 10)),
-		idx = chars[type].length;
-	
-	for(var i=0; i<len; i++){
-		str += chars[type][Math.floor(Math.random() * idx)];
-	}
-	
-	return str;
+global.generateRandomString = function genrs(type, max, fixed){
+  type = type in genrs._chars ? type : 'all';
+  
+  var str = '',
+      len = fixed ? max : Math.ceil(Math.random() * (max || 10)),
+      idx = genrs._chars[type].length;
+  
+  for (var i=0; i<len; i++){
+    str += genrs._chars[type][Math.floor(Math.random() * idx)];
+  }
+  
+  return str;
 };
+
+global.generateRandomString._chars = chars;
 })(window);
