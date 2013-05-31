8c2825b1581122e75675f6caf6cf6bd0aa745b78
Andrew Lee <starsthatfell@gmail.com>
2013-05-31 01:23:16 -0700

    updates AMD and CommonJS exposing as factory method

--- randomize.js
+++ randomize.js
@@ -1,37 +1,39 @@
-(function(){
-'use strict';
-
-var chars = {};
-
-chars.num      = '0123456789';
-chars.alphalc  = 'abcdefghijklmnopqrstuvwxyz';
-chars.alphauc  = chars.alphalc.toUpperCase();
-chars.alpha    = chars.alphalc + chars.alphauc;
-chars.alphanum = chars.alpha + chars.num;
-chars.special  = '!@#$%^&*()-_=+`~[]{}\\|;:\'"<>.,/?';
-chars.all      = chars.alphanum + chars.special;
-
-var randomize = function genrs(type, max, fixed){
-  type = type in genrs._chars ? type : 'all';
-  
-  var str = '',
-      len = fixed ? max : Math.ceil(Math.random() * (max || 10)),
-      idx = genrs._chars[type].length;
-  
-  for (var i=0; i<len; i++){
-    str += genrs._chars[type][Math.floor(Math.random() * idx)];
+(function(factory){
+  if (typeof define === 'function' && define.amd){
+    define(factory);
+  } else if (typeof module === 'object' && typeof module.exports === 'object'){
+    module.exports = factory();
+  } else {
+    this.randomize = factory();
   }
-  
-  return str;
-};
+}).call(this, function(){
+  'use strict';
 
-randomize._chars = chars;
+  var chars = {};
 
-if (typeof define === 'function' && define.amd){
-  define(function(){ return randomize; });
-} else if (typeof module === 'object' && typeof module.exports === 'object'){
-  module.exports = randomize;
-} else {
-  this.randomize = randomize;
-}
-}).call(this);
+  chars.num      = '0123456789';
+  chars.alphalc  = 'abcdefghijklmnopqrstuvwxyz';
+  chars.alphauc  = chars.alphalc.toUpperCase();
+  chars.alpha    = chars.alphalc + chars.alphauc;
+  chars.alphanum = chars.alpha + chars.num;
+  chars.special  = '!@#$%^&*()-_=+`~[]{}\\|;:\'"<>.,/?';
+  chars.all      = chars.alphanum + chars.special;
+
+  var randomize = function genrs(type, max, fixed){
+    type = type in genrs._chars ? type : 'all';
+    
+    var str = '',
+        len = fixed ? max : Math.ceil(Math.random() * (max || 10)),
+        idx = genrs._chars[type].length;
+    
+    for (var i=0; i<len; i++){
+      str += genrs._chars[type][Math.floor(Math.random() * idx)];
+    }
+    
+    return str;
+  };
+
+  randomize._chars = chars;
+
+  return randomize;
+});
